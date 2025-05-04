103d408c5855d52f4453a3b01dac7b108974e3e8
Sushantmg <sushantmg2004@gmail.com>
2025-05-04 21:37:02 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
+    <header className="flex flex-row bg-white shadow-md sticky top-0 z-50">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <ul className="flex flex-row space-x-6 text-gray-600 font-medium">
+          <li className="hover:text-blue-600 transition duration-200">
             <Link to={"/"}>Home</Link>
           </li>
           <li>
